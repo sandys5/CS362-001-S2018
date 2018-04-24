775adf3e9d41c6af4316e56cd5c84b482ecd94c1
sandys5 <sandys@oregonstate.edu>
2018-04-23 19:43:05 -0700

    assignment-2
    includes unit tests for assignment-2

---
+++ projects/sandys/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,44 @@
+/** A JUnit test class to test the class ApptTest. */
+
+package calendar;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalendarUtil;
+public class ApptTest  {
+
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+      String string0 = appt0.toString();
+      assertEquals(2, appt0.getRecurBy());
+      assertFalse(appt0.isRecurring());
+      assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+      assertEquals(0, appt0.getRecurIncrement());
+      appt0.setValid();
+  }
+@Test(timeout = 4000)
+ public void test01()  throws Throwable  {
+    int[] arr = new int[]{3, 3, 3};
+    Appt myAppt = new Appt(1,2,3,4,2001,"study","get to school","valid@comcast.net");
+    myAppt.setRecurrence(arr, 3, 3, 3);
+    assertTrue(myAppt.getValid());
+    assertEquals(3L, (long)myAppt.getRecurNumber());
+    assertEquals(3L, (long)myAppt.getRecurIncrement());
+    assertEquals(3L, (long)myAppt.getRecurBy());
+    assertEquals(true, myAppt.isRecurring());
+    myAppt.setRecurrence((int[])null, 3, 3, 3);
+    assertEquals(0L, (long)myAppt.getRecurDays().length);
+    assertEquals("valid@comcast.net", myAppt.getEmailAddress());
+    assertEquals(myAppt.getStartHour(),1);
+    assertEquals(myAppt.getStartMinute(),2);
+    assertEquals(myAppt.getStartDay(),3);
+    assertEquals(myAppt.getStartYear(),2001);
+    assertEquals(myAppt.getTitle(),"study");
+    assertEquals(myAppt.getDescription(), "get to school");
+
+}
+
+}
---
+++ projects/sandys/Assignment-2/Calendar/src/test/java/calendar/DataHandlerTest.java
@@ -0,0 +1,45 @@
+
+/** A JUnit test class to test the class DataHandler. */
+
+
+package calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalDay;
+import calendar.DataHandler;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+
+public class DataHandlerTest{
+
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+    Appt newAppt = new Appt(35,2,2020,"leap year", "doing some leaping","johns@gmail.com");
+    GregorianCalendar greg = new GregorianCalendar(2020, 2, 2);
+    assertTrue(greg.isLeapYear(greg.getWeekYear()));
+    int [] arr = new int [] {3,5,3};
+    newAppt.setRecurrence(arr,3,3,3);
+    assertFalse(newAppt.getValid());
+    assertEquals(newAppt.getRecurBy(), 3);
+    assertEquals(newAppt.getRecurIncrement(), 3);
+    assertEquals(newAppt.getRecurNumber(), 3);
+
+
+  }
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+    DataHandler data = new DataHandler("calendar.xml");
+    Appt valAppt = new Appt(25,3,2018,"valid", "doing valid", "smithj@gmail.com");
+    Appt invAppt = new Appt(3, 76, 23, 4, 2017, "title", "description", "email@gmail.com");
+    assertTrue(data.saveAppt(valAppt));
+    assertFalse(data.saveAppt(invAppt));
+    assertTrue(data.deleteAppt(valAppt));
+    assertFalse(data.deleteAppt(invAppt));
+
+  }
+
+}
